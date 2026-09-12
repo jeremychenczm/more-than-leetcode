5f8339c4d36df2cd955816697a076eab78ff6a89
People <chen.zhaom@northeastern.edu>
2026-09-12 16:31:52 -0700

    Time: 0 ms (100%), Space: 19.4 MB (34.55%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -2,9 +2,23 @@ class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
         if len(arr) == 2:
             return True
-        arr.sort()
-        diff = arr[1] - arr[0]
-        for i in range(2, len(arr)):
-            if arr[i] - arr[i - 1] != diff:
+
+        n = len(arr)
+        mn, mx = min(arr), max(arr)
+        if (mx - mn) % (n - 1) != 0:
+            return False
+        d = (mx - mn) // (n - 1)
+        if d == 0:
+            return True
+
+        seen = [False] * len(arr)
+        for num in arr:
+            if (num - mn) % d != 0:
                 return False
+            idx = (num - mn) // d
+            if seen[idx]:
+                return False
+            seen[idx] = True
+
         return True
+            
