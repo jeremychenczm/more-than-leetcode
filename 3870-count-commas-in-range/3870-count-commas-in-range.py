38779c7c1a3ada982a3908dadf90114746321083
People <chen.zhaom@northeastern.edu>
2026-09-12 22:20:09 -0700

    Time: 0 ms (100%), Space: 19.3 MB (45.37%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countCommas(self, n: int) -> int:
+        p = 1000
+        ans = 0
+        while p <= n:
+            ans += (n - p + 1)
+            p *= 1000
+        return ans
