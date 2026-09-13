41778c0c648e80e0c1f899a31df9e7e323431c19
People <chen.zhaom@northeastern.edu>
2026-09-13 12:55:06 -0700

    Time: 4 ms (51.11%), Space: 19.8 MB (14.5%) - LeetHub

--- 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.py
@@ -1,35 +1,30 @@
-from collections import defaultdict
-
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
         parent = list(range(n))
         rank = [0] * n
-
-        def find(x: int) -> int:
-            while parent[x] != x:
+        def find(x):
+            while x != parent[x]:
                 parent[x] = parent[parent[x]]
                 x = parent[x]
             return x
-
-        def union(a: int, b: int) -> bool:
+        def union(a, b):
             ra, rb = find(a), find(b)
             if ra == rb:
                 return False
             if rank[ra] < rank[rb]:
                 ra, rb = rb, ra
             parent[rb] = ra
+
             if rank[ra] == rank[rb]:
                 rank[ra] += 1
             return True
 
-        # sort the timestamp
         logs.sort(key=lambda log: log[0])
         remaining = n
-
-        for timestamp, a, b in logs:
+        for ts, a, b in logs:
             if union(a, b):
                 remaining -= 1
                 if remaining == 1:
-                    return timestamp
+                    return ts
 
         return -1
