0e5acad5ac2985bf164976487faa01514d57ee1f
People <chen.zhaom@northeastern.edu>
2026-09-14 13:16:20 -0700

    Time: 70 ms (34.26%), Space: 35.2 MB (65.51%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.py
@@ -1,18 +1,7 @@
 class Solution:
     def uniformArray(self, nums1: list[int]) -> bool:
-        n = len(nums1)
-        min_odd = (n, float('inf'))
-        odd = False
-        for idx, num in enumerate(nums1):
-            if num % 2 != 0:
-                odd = True
-                if num < min_odd[1]:
-                    min_odd = (idx, num)
-
-        if not odd:
+        odds = [x for x in nums1 if x % 2]
+        if not odds:
             return True
-
-        for idx, num in enumerate(nums1):
-            if num % 2 == 0 and not (idx != min_odd[0] and num - min_odd[1] >= 1):
-                return False
-        return True
+        min_odd = min(odds)
+        return all(x > min_odd for x in nums1 if x % 2 == 0)
