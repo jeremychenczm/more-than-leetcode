2ffd509ceb4d2df2645f66c59fda2a0b5505327b
People <chen.zhaom@northeastern.edu>
2026-09-14 15:07:10 -0700

    Time: 87 ms (53.84%), Space: 31.8 MB (67.25%) - LeetHub

---
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        n = len(heights)
+        ans = [0] * n
+
+        st = []
+        for i in range(n - 1, -1, -1):
+            while st and heights[i] > st[-1]:
+                st.pop()
+                ans[i] += 1
+            if st: 
+                ans[i] += 1
+            st.append(heights[i])
+
+        return ans
