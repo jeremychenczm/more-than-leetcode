80ca15020a8af82f4ee65a3e25b2f9300eb90263
People <chen.zhaom@northeastern.edu>
2026-09-15 14:16:18 -0700

    Time: 0 ms (100%), Space: 19.5 MB (46.11%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,22 +1,7 @@
 from collections import Counter
-"""手写堆"""
-class Node:
-    def __init__(self, freq, word):
-        self.freq = freq
-        self.word = word
-    def __lt__(self, other):
-        if self.freq != other.freq:
-            return self.freq < other.freq
-        return self.word > other.word
+import heapq
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         cnt = Counter(words)
-        heap = []
-        for word, freq in cnt.items():
-            heapq.heappush(heap, Node(freq, word))
-            if len(heap) > k:
-                heapq.heappop(heap)
-        
-        ans = [heapq.heappop(heap).word for _ in range(len(heap))]
-        return ans[::-1]
+        return heapq.nsmallest(k, cnt, key=lambda w: (-cnt[w], w))        
