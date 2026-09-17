ad05d7ac7cbd79ee107b31b13c4989d8a24af219
People <chen.zhaom@northeastern.edu>
2026-09-16 21:37:26 -0700

    Time: 46 ms (72.6%), Space: 36.6 MB (67.13%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,12 +1,14 @@
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        s = set(nums)
+    def longestConsecutive(self, nums: list[int]) -> int:
+        """找起点才不会超时"""
         ans = 0
-        for k in s:
-            if k - 1 not in s:
-                step = 1
-                while k + step in s:
-                    step += 1
-                ans = max(ans, step)
-
+        nums_set = set(nums)
+        for num in nums_set:
+            # 有更小的说明当前num不是起点
+            if num - 1 in nums_set:
+                continue
+            end = num
+            while end + 1 in nums_set:
+                end += 1
+            ans = max(ans, end - num + 1)
         return ans
