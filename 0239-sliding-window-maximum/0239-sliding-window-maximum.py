894065578c269d92cb5d483789fcc4493d8987e0
People <chen.zhaom@northeastern.edu>
2026-09-18 13:03:56 -0700

    Time: 182 ms (71.56%), Space: 34.6 MB (86.76%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,13 +1,19 @@
+from collections import deque
 class Solution:
     def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-        l = r = 0
-        st = []
+        dq = deque()
         ans = []
-        while r < len(nums):
-            if not st or st[-1] < nums[r]:
-                st.append(nums[r])
-            if r - l + 1 == k:
-                ans.append(st[-1])
-                l += 1
-            r += 1
+        n = len(nums)
+
+        for i in range(n):
+            # 先查队列头部元素是否超出窗口
+            if dq and dq[0] <= i - k:
+                dq.popleft()
+            # 维护单调递增队列
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+            dq.append(i)
+            if i >= k - 1:
+                ans.append(nums[dq[0]])
+        
         return ans
