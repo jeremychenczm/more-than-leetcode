bcf870195453be7875965db775257cbc90b81088
People <chen.zhaom@northeastern.edu>
2026-09-18 13:49:30 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,16 +1,21 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
+        # 从左往右观察到，一旦元素开始递减，那么说明相邻左侧元素遇到右边界，可以开始结算
+        # 由此推出要维护一个单调递增序列，然后不断弹栈结算栈内元素
         st = []
-        ans = 0
+        n = len(heights)
+        ans = -1
 
-        for r_bound in range(n + 1):
-            cur_h = 0 if r_bound == n else heights[r_bound]
-            while st and cur_h < heights[st[-1]]:
+        for i in range(n + 1):
+            # cur_h作为比较元素，来确认栈内元素是否要结算
+            h = 0 if i == n else heights[i]
+            while st and h < heights[st[-1]]:
                 cur_idx = st.pop()
-                l_bound = st[-1] if st else -1
-                area = (r_bound - l_bound - 1) * heights[cur_idx]
+                left_bound = st[-1] if st else -1
+                right_bound = i
+                area = (right_bound - left_bound - 1) * heights[cur_idx]
                 ans = max(ans, area)
-            st.append(r_bound)
-        
+            
+            st.append(i)
+
         return ans
