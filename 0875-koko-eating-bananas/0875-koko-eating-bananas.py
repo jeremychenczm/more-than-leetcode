5e2b3101825eab3711999c25cb668a935edddf9c
People <chen.zhaom@northeastern.edu>
2026-09-19 15:18:21 -0700

    Time: 171 ms (48.62%), Space: 20.6 MB (82.22%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,18 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        # speed == [1, max:piles[i]]
-        l, r = 1, max(piles)
-        while l <= r:
-            mid = (l + r) // 2
-            t = self.cntTime(mid, piles)
-            if t > h:
-                l = mid + 1
-            else:
-                r = mid - 1
-        return l
+        # eat all but with minimum k within h hours
+        # slowly, so: 1 <= k <= max(piles)
 
-    def cntTime(self, speed, piles):
-        t = 0
-        for p in piles:
-            t += (p + speed - 1) // speed
-        return t
+        def can_finish(speed):
+            hours = 0
+            for p in piles:
+                hours += (p + speed - 1) // speed
+            return True if hours <= h else False
+
+        l, r = 1, max(piles)
+        while l < r:
+            mid = (l + r) // 2
+            if can_finish(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
