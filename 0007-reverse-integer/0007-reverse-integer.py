111837cc6f2541941363553568b4990cada96796
People <chen.zhaom@northeastern.edu>
2026-09-19 12:54:31 -0700

    Time: 0 ms (100%), Space: 19.5 MB (10.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,16 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        is_neg = False
-        if x < 0:
-            is_neg = True
-            x *= -1
-        
+        INT_MAX = 2 ** 31 - 1
+        is_neg = x < 0
+        x = abs(x)
+
         num = 0
         while x > 0:
             digit = x % 10
             x //= 10
-            if (num > (2 ** 31 - 1) // 10) or (num == (2 ** 31 - 1) // 10 and digit > 7):
+            if num > INT_MAX // 10 or (num == INT_MAX // 10 and digit > 7):
                 return 0
             num = num * 10 + digit
 
-        return num if not is_neg else -num
+        return -num if is_neg else num
