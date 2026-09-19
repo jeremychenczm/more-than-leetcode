fea41261938a109170d6ac5a36acc6a6f857714c
People <chen.zhaom@northeastern.edu>
2026-09-19 14:53:09 -0700

    Time: 331 ms (88.53%), Space: 19.5 MB (16.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,21 +1,22 @@
 class Solution:
-    def threeSumClosest(self, nums: List[int], target: int) -> int:
-        nums.sort()
+    def threeSumClosest(self, nums: list[int], target: int) -> int:
+        # ans is closet to target
+        sorted_arr = sorted(nums)
         n = len(nums)
-        diff = float("inf")
-        for i in range(n):
-            a = nums[i]
+
+        diff = float('inf')
+        for i, num in enumerate(sorted_arr):
             l, r = i + 1, n - 1
             while l < r:
-                b, c = nums[l], nums[r]
-                total = a + b + c
+                total = sorted_arr[l] + sorted_arr[r] + sorted_arr[i]
                 if abs(target - total) < abs(diff):
                     diff = target - total
                 if total < target:
                     l += 1
                 else:
                     r -= 1
-
+            
             if diff == 0:
                 break
+        
         return target - diff
