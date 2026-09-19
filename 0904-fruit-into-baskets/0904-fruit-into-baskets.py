c78f2e786643ed97bb0667071a740e8d2e6e64d1
People <chen.zhaom@northeastern.edu>
2026-09-19 14:44:45 -0700

    Time: 202 ms (22.71%), Space: 25.2 MB (92.86%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,21 +1,20 @@
 from collections import defaultdict
 class Solution:
-    def totalFruit(self, fruits: List[int]) -> int:
-        basket = defaultdict(int)
-        ans = 0
-        left = right = 0
+    def totalFruit(self, fruits: list[int]) -> int:
+        start = end = 0
         n = len(fruits)
+        types = defaultdict(int)
+        ans = 0
 
-        while right < n:
-            basket[fruits[right]] += 1
+        while end < n:
+            types[fruits[end]] += 1
+            while len(types) > 2:
+                remove = fruits[start]
+                types[remove] -= 1
+                if types[remove] == 0:
+                    del types[remove]
+                start += 1
+            ans = max(ans, end - start + 1)
+            end += 1
 
-            while len(basket) > 2:
-                basket[fruits[left]] -= 1
-                if basket[fruits[left]] == 0:
-                    del basket[fruits[left]]
-                left += 1
-            ans = max(ans, right - left + 1)
-            
-            right += 1
-        
         return ans
