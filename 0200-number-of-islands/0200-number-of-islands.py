292b4b6cf3b6c14ffe1961ef704be6b07a231a70
People <chen.zhaom@northeastern.edu>
2026-09-20 20:31:16 -0700

    Time: 248 ms (45.93%), Space: 25.5 MB (29.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,45 @@
-dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        self.cnt = 0
+
+    def find(self, x):
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
+        return x
+
+    def union(self, a, b):
+        ra, rb = self.find(a), self.find(b)
+        if ra == rb:
+            return
+        if self.rank[ra] < self.rank[rb]:
+            ra, rb = rb, ra
+        self.parent[rb] = ra
+        if self.rank[ra] == self.rank[rb]:
+            self.rank[ra] += 1
+        self.cnt -= 1
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        def dfs(x, y):
-            if not(0 <= x < m and 0 <= y < n) or grid[x][y] != "1":
-                return
-            
-            grid[x][y] = "0"
-            for d in dirs:
-                nx, ny = d[0] + x, d[1] + y
-                dfs(nx, ny)
+        uf = UnionFind(m * n)
 
-        ans = 0
+        # 先统计要处理多少个 ‘1’
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
-                    dfs(i, j)
-                    ans += 1
+                if grid[i][j] == '1':
+                    uf.cnt += 1
 
-        return ans
-                
+        # 进行联通
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    idx = i * n + j
+                    if i + 1 < m and grid[i + 1][j] == '1':
+                        uf.union(idx, (i + 1) * n + j)
+                    if j + 1 < n and grid[i][j + 1] == '1':
+                        uf.union(idx, i * n + j + 1)
+
+        return uf.cnt
