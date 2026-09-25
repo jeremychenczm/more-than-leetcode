8172ef1baa0c014b75bd9faa1a5c0772ea3d1168
People <chen.zhaom@northeastern.edu>
2026-09-25 14:38:44 -0700

    Time: 7 ms (34.04%), Space: 20.7 MB (14.65%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.py
+++ 0174-dungeon-game/0174-dungeon-game.py
@@ -1,42 +1,26 @@
 class Solution:
     def calculateMinimumHP(self, dungeon: list[list[int]]) -> int:
-        # 我首先想到候选答案是升序的，[1, total_neg]
-        # 尝试用答案二分
-
         m, n = len(dungeon), len(dungeon[0])
 
-        def canRescue(start_health: int) -> bool:
-            # dp[i][j]：以 start_health 出发，走到 (i, j) 时能保有的最大血量
-            # -1 表示这个格子在血量 > 0 的前提下不可达
-            dp = [[-1] * n for _ in range(m)]
-            for i in range(m):
-                for j in range(n):
-                    if i == 0 and j == 0:
-                        h = start_health + dungeon[i][j]
-                    else:
-                        best_prev = -1
-                        if i > 0 and dp[i - 1][j] > 0:
-                            best_prev = max(best_prev, dp[i - 1][j])
-                        if j > 0 and dp[i][j - 1] > 0:
-                            best_prev = max(best_prev, dp[i][j - 1])
-                        if best_prev <= 0:
-                            continue
-                        h = best_prev + dungeon[i][j]
-                    if h > 0:
-                        dp[i][j] = h
-            return dp[m - 1][n - 1] > 0
+        # dp[i][j]：站在 (i, j) 这一格，进入这一格之前至少需要多少血量，
+        # 才能保证从这里出发一路走到终点，且全程血量 > 0
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m, -1, -1):
+            for j in range(n, -1, -1):
+                dp[i][j] = float('inf')
 
-        neg_total = 0
-        for row in dungeon:
-            for v in row:
-                if v < 0:
-                    neg_total += v
+        # 再往前一步就是终点，至少需要留 1 点血
+        dp[m - 1][n] = 1
+        dp[m][n - 1] = 1
 
-        l, r = 1, max(1, abs(neg_total) + 1)
-        while l < r:
-            mid = (l + r) // 2
-            if canRescue(mid):
-                r = mid
-            else:
-                l = mid + 1
-        return l
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if i == m - 1 and j == n - 1:
+                    need = 1
+                else:
+                    need = min(dp[i + 1][j], dp[i][j + 1])
+                # 进入 (i,j) 前需要的血量 = 走完这格之后至少还剩 need，
+                # 反推回来进入前需要 need - dungeon[i][j]，且至少是 1
+                dp[i][j] = max(1, need - dungeon[i][j])
+
+        return dp[0][0]
